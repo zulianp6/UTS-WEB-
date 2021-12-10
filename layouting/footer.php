4c57cfce82adf2fadee3b342c4a771f0f26b780f
zulianp6 <94003720+zulianp6@users.noreply.github.com>
2021-12-10 22:03:01 +0700

    Add files via upload

---
+++ layouting/footer.php
@@ -0,0 +1,5 @@
+<footer>
+		<div class="container">
+			<small>Copyright © 2020 - zulia nur permatasari. All Rights Reserved.</small>
+		</div>
+	</footer>
---
+++ layouting/navigation.php
@@ -0,0 +1,12 @@
+<header>
+		<div class="container">
+			<h1><a href="index.html">POSYANDU TULIP
+			</a></h1>
+			<ul>
+				<li class="active"><a href="index.html">HOME</a></li>
+				<li><a href="jadwal.html">JADWAL</a></li>
+				<li><a href="history.html">HISTORY</a></li>
+				<li><a href="pengurus.html">PENGURUS</a></li>
+			</ul>
+		</div>
+	</header>
